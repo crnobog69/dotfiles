63195ef2ed0046d42e75dba588e2f55d3162c45c
crnobog <carskalavra@proton.me>
2025-02-19 22:04:41 +0100

    ❄️

--- scripts/entropy.sh
+++ scripts/entropy.sh
@@ -39,221 +39,248 @@ if ! gum confirm --prompt.foreground="#f9e2af" --selected.background="#e78284" -
     echo -e "${RED}Чишћење је отказано.${NC}"
     exit 1
 fi
-echo "Чишћење система је покренуто..."
+echo -e "${GREEN} Чишћење система је покренуто...${NC}"
 
 echo
 
-echo ===========================================================================
-echo -e "${RED}  ${NC}${GREEN}Чишћење система...${NC}"
-echo ===========================================================================
+# Избор опције за чишћење
+choice=$(gum choose\
+    --header=" Избор:" \
+    --item.foreground="#b4befe" \
+    --cursor.foreground="#e78284" \
+    --header.foreground="#f9e2af" \
+    --selected.foreground="#b4befe" \
+    " Менаџери пакета + Програми" \
+    " Само Менаџери пакета" \
+    "󰻀 Само Програми" \
+    "󰈆 Излаз")
+echo "$(gum style --foreground "#f9e2af" "Изабрана опција: $choice")"
 
 echo
 
-echo -e "=== ${RED}󰏖 | Менаџери пакета ${NC} =================================================="
+if [ "$choice" == "󰈆 Излаз" ]; then
+    exit 1;
+fi
 
-echo
+if [ "$choice" != "󰻀 Само Програми" ]; then
+    echo ===========================================================================
+    echo -e "${RED}  ${NC}${GREEN}Чишћење система...${NC}"
+    echo ===========================================================================
 
-echo -e "${GREEN} Чишћење кеша Pacman...${NC}"
-sudo pacman -Sc --noconfirm
-
-echo -e "${GREEN} Чишћење кеша yay...${NC}"
-yay -Sc --noconfirm
-
-echo -e "${GREEN} Уклањање неупотребљених зависности yay...${NC}"
-yay -Yc --noconfirm
-
-echo
-
-echo -e "=== ${RED}󰂺 | Дневник${NC} ==========================================================="  
-
-echo
-
-echo -e "${YELLOW} Величина /var/log/journal директоријума пре чишћења:${NC}"
-du -sh /var/log/journal | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-
-echo
-
-echo -e "${GREEN} Чишћење journal логова старијих од 2 недеље...${NC}"
-sudo journalctl --vacuum-time=2weeks
-
-echo 
-
-echo -e "${YELLOW} Величина /var/log/journal директоријума:${NC}"
-du -sh /var/log/journal | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-
-echo
-
-echo -e "=== ${RED}󰃨 | Кеш${NC} ==============================================================="
-
-echo
-
-echo -e "${YELLOW} Величина ~/.cache директоријума пре чишћења:${NC}"
-du -sh ~/.cache | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-
-echo
-
-echo -e "${GREEN} Брисање корисничког кеша...${NC}"
-rm -rf ~/.cache/*
-
-echo
-
-echo -e "${YELLOW} Величина ~/.cache директоријума:${NC}"
-du -sh ~/.cache | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-
-echo
-
-echo -e "=== ${RED} | Brave${NC} ============================================================="
-
-echo
-
-if [ -d ~/.config/BraveSoftware/Brave-Browser/Default/Service\ Worker/CacheStorage/ ]; then
-    echo -e "${YELLOW} Величина кеша Brave:${NC}"
-    du -sh ~/.config/BraveSoftware/Brave-Browser/Default/Service\ Worker/CacheStorage/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
     echo
-    echo -e "${GREEN} Брисање кеша Brave...${NC}"
-    rm -rf ~/.config/BraveSoftware/Brave-Browser/Default/Service\ Worker/CacheStorage/*
+
+    echo -e "=== ${RED}󰏖 | Менаџери пакета ${NC} =================================================="
+
     echo
-    echo -e "${YELLOW} Величина кеша Brave:${NC}"
-    du -sh ~/.config/BraveSoftware/Brave-Browser/Default/Service\ Worker/CacheStorage/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-else
-    echo -e "${RED}Brave није инсталиран${NC}"
+
+    echo -e "${GREEN} Чишћење кеша Pacman...${NC}"
+    sudo pacman -Sc --noconfirm
+
+    echo -e "${GREEN} Чишћење кеша yay...${NC}"
+    yay -Sc --noconfirm
+
+    echo -e "${GREEN} Уклањање неупотребљених зависности yay...${NC}"
+    yay -Yc --noconfirm
+
+    echo
 fi
 
 echo
 
-echo -e "=== ${RED} | Vesktop${NC} ==========================================================="
+if [ "$choice" != " Само Менаџери пакета" ]; then
+    echo -e "=== ${RED}󰂺 | Дневник${NC} ==========================================================="  
 
-echo
+    echo
 
-if [ -d ~/.config/vesktop/sessionData/ ]; then
-    echo -e "${YELLOW} Величина кеша Vesktop:${NC}"
-    du -sh ~/.config/vesktop/sessionData/Cache/Cache_Data/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-    du -sh ~/.config/vesktop/sessionData/Code\ Cache/js/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+    echo -e "${YELLOW} Величина /var/log/journal директоријума пре чишћења:${NC}"
+    du -sh /var/log/journal | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
     echo
-    echo -e "${GREEN} Брисање кеша Vesktop...${NC}"
-    rm -rf ~/.config/vesktop/sessionData/Cache/Cache_Data/*
-    rm -rf ~/.config/vesktop/sessionData/Code\ Cache/js/*
+
+    echo -e "${GREEN} Чишћење journal логова старијих од 2 недеље...${NC}"
+    sudo journalctl --vacuum-time=2weeks
+
+    echo 
+
+    echo -e "${YELLOW} Величина /var/log/journal директоријума:${NC}"
+    du -sh /var/log/journal | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
     echo
-    echo -e "${YELLOW} Величина кеша Vesktop:${NC}"
-    du -sh ~/.config/vesktop/sessionData/Cache/Cache_Data/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-    du -sh ~/.config/vesktop/sessionData/Code\ Cache/js/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-else
-    echo -e "${RED} Vesktop није инсталиран${NC}"
+
+    echo -e "=== ${RED}󰃨 | Кеш${NC} ==============================================================="
+
+    echo
+
+    echo -e "${YELLOW} Величина ~/.cache директоријума пре чишћења:${NC}"
+    du -sh ~/.cache | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
+    echo
+
+    echo -e "${GREEN} Брисање корисничког кеша...${NC}"
+    rm -rf ~/.cache/*
+
+    echo
+
+    echo -e "${YELLOW} Величина ~/.cache директоријума:${NC}"
+    du -sh ~/.cache | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
+    echo
+
+    echo -e "=== ${RED} | Brave${NC} ============================================================="
+
+    echo
+
+    if [ -d ~/.config/BraveSoftware/Brave-Browser/Default/Service\ Worker/CacheStorage/ ]; then
+        echo -e "${YELLOW} Величина кеша Brave:${NC}"
+        du -sh ~/.config/BraveSoftware/Brave-Browser/Default/Service\ Worker/CacheStorage/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        echo
+        echo -e "${GREEN} Брисање кеша Brave...${NC}"
+        rm -rf ~/.config/BraveSoftware/Brave-Browser/Default/Service\ Worker/CacheStorage/*
+        echo
+        echo -e "${YELLOW} Величина кеша Brave:${NC}"
+        du -sh ~/.config/BraveSoftware/Brave-Browser/Default/Service\ Worker/CacheStorage/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+    else
+        echo -e "${RED}Brave није инсталиран${NC}"
+    fi
+
+    echo
+
+    echo -e "=== ${RED} | Vesktop${NC} ==========================================================="
+
+    echo
+
+    if [ -d ~/.config/vesktop/sessionData/ ]; then
+        echo -e "${YELLOW} Величина кеша Vesktop:${NC}"
+        du -sh ~/.config/vesktop/sessionData/Cache/Cache_Data/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        du -sh ~/.config/vesktop/sessionData/Code\ Cache/js/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        echo
+        echo -e "${GREEN} Брисање кеша Vesktop...${NC}"
+        rm -rf ~/.config/vesktop/sessionData/Cache/Cache_Data/*
+        rm -rf ~/.config/vesktop/sessionData/Code\ Cache/js/*
+        echo
+        echo -e "${YELLOW} Величина кеша Vesktop:${NC}"
+        du -sh ~/.config/vesktop/sessionData/Cache/Cache_Data/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        du -sh ~/.config/vesktop/sessionData/Code\ Cache/js/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+    else
+        echo -e "${RED} Vesktop није инсталиран${NC}"
+    fi
+
+    echo
+
+    echo -e "=== ${RED}󰨞 | VS Code${NC} ==========================================================="
+
+    echo
+
+    if [ -d ~/.config/Code/CachedData/ ]; then
+        echo -e "${YELLOW} Величина кеша VS Code:${NC}"
+        du -sh ~/.config/Code/CachedData/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        echo
+        echo -e "${GREEN} Брисање кеша VS Code...${NC}"
+        rm -rf ~/.config/Code/CachedData/*
+        echo
+        echo -e "${YELLOW} Величина кеша VS Code:${NC}"
+        du -sh ~/.config/Code/CachedData/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+    else
+        echo -e "${RED} VS Code није инсталиран${NC}"
+    fi
+
+    echo
+
+    echo -e "=== ${RED}󰇀 | Cursor${NC} ============================================================"
+
+    echo
+
+    if [ -d ~/.config/Cursor/CachedData/ ]; then
+        echo -e "${YELLOW} Величина кеша Cursor:${NC}"
+        du -sh ~/.config/Cursor/CachedData/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        echo
+        echo -e "${GREEN} Брисање кеша Cursor...${NC}"
+        rm -rf ~/.config/Cursor/CachedData/*
+        echo
+        echo -e "${YELLOW} Величина кеша Cursor:${NC}"
+        du -sh ~/.config/Cursor/CachedData/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+    else
+        echo -e "${RED} Cursor није инсталиран${NC}"
+    fi
+
+    echo
+
+    echo -e "=== ${RED} | Bun${NC} ==============================================================="
+
+    echo
+
+    if [ -d ~/.bun/install/cache ]; then
+        echo -e "${YELLOW} Величина кеша Bun:${NC}"
+        du -sh ~/.bun/install/cache/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        echo
+        echo -e "${GREEN} Брисање кеша Bun...${NC}"
+        rm -rf ~/.bun/install/cache/*
+        echo
+        echo -e "${YELLOW} Величина кеша Bun:${NC}"
+        du -sh ~/.bun/install/cache/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+    else
+        echo -e "${RED} Bun није инсталиран${NC}"
+    fi
+    echo
+
+    echo -e "=== ${RED} | Gradle${NC} ============================================================"
+
+    echo
+
+    if [ -d ~/.gradle/ ]; then
+        echo -e "${YELLOW} Величина кеша и врапера Gradle:${NC}"
+        du -sh ~/.gradle/caches/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        du -sh ~/.gradle/wrapper/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        echo
+        echo -e "${GREEN} Брисање кеша и врапера Gradle...${NC}"
+        rm -rf ~/.gradle/caches/*
+        rm -rf ~/.gradle/wrapper/*
+        echo
+        echo -e "${YELLOW} Величина кеша и врапера Gradle:${NC}"
+        du -sh ~/.gradle/caches/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+        du -sh ~/.gradle/wrapper/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+    else
+        echo -e "${RED}Gradle није инсталиран${NC}"
+    fi
+
+    echo
+
+    echo -e "=== ${RED} | Канта${NC} ============================================================="
+
+    echo 
+
+    echo -e "${YELLOW} Величина канте пре чишћења:${NC}"
+    du -sh ~/.local/share/Trash/files/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
+    rm -rf ~/.local/share/Trash/files/*
+
+    echo
+
+    echo -e "${YELLOW} Величина канте након чишћења:${NC}"
+    du -sh ~/.local/share/Trash/files/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
+    echo
+
+    echo -e "=== ${RED} | Остало${NC} ============================================================"
+
+
+    echo
+
+    echo -e "${YELLOW} Величина ~/.config директоријума:${NC}"
+    du -sh ~/.config | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
+    echo
+
+    echo -e "${YELLOW} Величина ~/.local директоријума:${NC}"
+    du -sh ~/.local | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
+    echo
+
+    echo -e "${YELLOW} Величина ~/.cargo директоријума:${NC}"
+    du -sh ~/.cargo | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
+
+    echo
 fi
-
-echo
-
-echo -e "=== ${RED}󰨞 | VS Code${NC} ==========================================================="
-
-echo
-
-if [ -d ~/.config/Code/CachedData/ ]; then
-    echo -e "${YELLOW} Величина кеша VS Code:${NC}"
-    du -sh ~/.config/Code/CachedData/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-    echo
-    echo -e "${GREEN} Брисање кеша VS Code...${NC}"
-    rm -rf ~/.config/Code/CachedData/*
-    echo
-    echo -e "${YELLOW} Величина кеша VS Code:${NC}"
-    du -sh ~/.config/Code/CachedData/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-else
-    echo -e "${RED} VS Code није инсталиран${NC}"
-fi
-
-echo
-
-echo -e "=== ${RED}󰇀 | Cursor${NC} ============================================================"
-
-echo
-
-if [ -d ~/.config/Cursor/CachedData/ ]; then
-    echo -e "${YELLOW} Величина кеша Cursor:${NC}"
-    du -sh ~/.config/Cursor/CachedData/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-    echo
-    echo -e "${GREEN} Брисање кеша Cursor...${NC}"
-    rm -rf ~/.config/Cursor/CachedData/*
-    echo
-    echo -e "${YELLOW} Величина кеша Cursor:${NC}"
-    du -sh ~/.config/Cursor/CachedData/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-else
-    echo -e "${RED} Cursor није инсталиран${NC}"
-fi
-
-echo
-
-echo -e "=== ${RED} | Bun${NC} ==============================================================="
-
-echo
-
-if [ -d ~/.bun/install/cache ]; then
-    echo -e "${YELLOW} Величина кеша Bun:${NC}"
-    du -sh ~/.bun/install/cache/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-    echo
-    echo -e "${GREEN} Брисање кеша Bun...${NC}"
-    rm -rf ~/.bun/install/cache/*
-    echo
-    echo -e "${YELLOW} Величина кеша Bun:${NC}"
-    du -sh ~/.bun/install/cache/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-else
-    echo -e "${RED} Bun није инсталиран${NC}"
-fi
-echo
-
-echo -e "=== ${RED} | Gradle${NC} ============================================================"
-
-echo
-
-if [ -d ~/.gradle/ ]; then
-    echo -e "${YELLOW} Величина кеша и врапера Gradle:${NC}"
-    du -sh ~/.gradle/caches/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-    du -sh ~/.gradle/wrapper/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-    echo
-    echo -e "${GREEN} Брисање кеша и врапера Gradle...${NC}"
-    rm -rf ~/.gradle/caches/*
-    rm -rf ~/.gradle/wrapper/*
-    echo
-    echo -e "${YELLOW} Величина кеша и врапера Gradle:${NC}"
-    du -sh ~/.gradle/caches/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-    du -sh ~/.gradle/wrapper/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-else
-    echo -e "${RED}Gradle није инсталиран${NC}"
-fi
-
-echo
-
-echo -e "=== ${RED} | Канта${NC} ============================================================="
-
-echo 
-
-echo -e "${YELLOW} Величина канте пре чишћења:${NC}"
-du -sh ~/.local/share/Trash/files/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-
-rm -rf ~/.local/share/Trash/files/*
-
-echo
-
-echo -e "${YELLOW} Величина канте након чишћења:${NC}"
-du -sh ~/.local/share/Trash/files/ | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-
-echo
-
-echo -e "=== ${RED} | Остало${NC} ============================================================"
-
-
-echo
-
-echo -e "${YELLOW} Величина ~/.config директоријума:${NC}"
-du -sh ~/.config | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-
-echo
-
-echo -e "${YELLOW} Величина ~/.local директоријума:${NC}"
-du -sh ~/.local | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
-
-echo
-
-echo -e "${YELLOW} Величина ~/.cargo директоријума:${NC}"
-du -sh ~/.cargo | awk '{print " \033[31m"$1"\033[0m \033[38;2;180;191;254m"$2"\033[0m"}'
 
 echo
 
